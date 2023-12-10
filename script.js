0beec0be3f3a93e97aca68ac42b07a90882f4e57
Kaasjongetje <cas.hukkelhoven@gmail.com>
2023-12-10 14:08:21 +0100

    Fourth Commit

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
 
+console.log('hello')
+
 const url = "https://jscgkvmmxlzvvnpyldma.supabase.co"
 const key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpzY2drdm1teGx6dnZucHlsZG1hIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDE0MzY1MjYsImV4cCI6MjAxNzAxMjUyNn0.8fDSoj_BrFTqZuhTAROwcvOM7pnnTSotypCT1BaQzJQ"
 
